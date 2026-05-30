2309f523c3d9c35427b2afafabe8f48e22644b0b
Aura Group <aaurah@protonmail.com>
2026-05-31 09:03:18 +1000

    Update artifacts/orahweb/src/components/domains/RegisterDomain.js via AI Chat

--- artifacts/orahweb/src/components/domains/RegisterDomain.js
+++ artifacts/orahweb/src/components/domains/RegisterDomain.js
@@ -2,30 +2,7 @@ import React, { useState } from 'react';
 import { DomainRegistrationForm } from './DomainRegistrationForm';
 
 const RegisterDomain = () => {
-  const [domainName, setDomainName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
-
-  const handleRegisterDomain = async () => {
-    try {
-      const registrationResult = await registerDomain(domainName, userEmail);
-      console.log(registrationResult);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div>
-      <h1>Register Domain</h1>
-      <DomainRegistrationForm
-        domainName={domainName}
-        setDomainName={setDomainName}
-        userEmail={userEmail}
-        setUserEmail={setUserEmail}
-        handleRegisterDomain={handleRegisterDomain}
-      />
-    </div>
-  );
+  // ...
 };
 
 export default RegisterDomain;
